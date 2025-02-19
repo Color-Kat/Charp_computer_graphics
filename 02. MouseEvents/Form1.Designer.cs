5ef873897be124c29558730560ba528dffcf693a
ColorKat <katik7979@gmail.com>
2025-02-19 15:25:49 +0300

    graphics

---
+++ 02. MouseEvents/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace MouseEvents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.runawayButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // runawayButton
+            // 
+            this.runawayButton.Location = new System.Drawing.Point(584, 328);
+            this.runawayButton.Name = "runawayButton";
+            this.runawayButton.Size = new System.Drawing.Size(197, 93);
+            this.runawayButton.TabIndex = 0;
+            this.runawayButton.Text = "Press Me";
+            this.runawayButton.UseVisualStyleBackColor = true;
+            this.runawayButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1318, 818);
+            this.Controls.Add(this.runawayButton);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runawayButton;
+    }
+}
+
---
+++ 02. MouseEvents/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseEvents
+{
+    public partial class Form1 : Form
+    {
+        private Label label;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.runawayButton.MouseEnter += RunAway;
+
+            label = new Label();
+            label.Text = "Never Catch!";
+            label.Visible = false;
+            this.Controls.Add(label);
+
+        }
+
+        private void RunAway(object sender, EventArgs e)
+        {
+
+            Random random = new Random();
+
+            // New random position
+            runawayButton.Location = new Point(
+                random.Next(this.ClientSize.Width - runawayButton.Width),
+                random.Next(this.ClientSize.Height - runawayButton.Height)
+            );
+
+            runawayButton.BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+
+            label.Visible = true;
+            label.Location = new Point(
+                random.Next(this.ClientSize.Width - runawayButton.Width),
+                random.Next(this.ClientSize.Height - runawayButton.Height)
+            );
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
